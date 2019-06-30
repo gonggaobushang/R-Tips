86a4408fa2a5bd15a91a53bb0a512a21c332b787
gonggaobushang <47070965+gonggaobushang@users.noreply.github.com>
2019-06-30 17:32:45 +0800

    Update install package.R

--- install package.R
+++ install package.R
@@ -1,9 +1,10 @@
+#有些包不翻墙很难安装
+#又有些在cran上已经不存在的包如Rgraphviz，常规方式安装不了
+
+#3.5及以上的R已经不支持以下方式
 source("http://bioconductor.org/biocLite.R") 
 biocLite("Rgraphviz")
-
-
-
-
+#可以用Bioconductor包安装其他包
 if (!requireNamespace("BiocManager", quietly = TRUE))
   install.packages("BiocManager")
 

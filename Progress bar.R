d8ec3b787f67cd7c880c07d7eeb81b449a070d1d
gonggaobushang <47070965+gonggaobushang@users.noreply.github.com>
2019-07-02 15:30:39 +0800

    Update and rename 进度条.R to Progress bar.R

--- Progress bar.R
+++ Progress bar.R
@@ -1,11 +1,8 @@
-# https://mp.weixin.qq.com/s/4t3I88Ddqp3wCMCG0BbeOw
+#设置一个进度条
 library(tcltk2)  
 u <- 1:4000  
-
 #开启进度条  
-
 pb <- tkProgressBar("左上角的标题","中间，达到%", 0, 333)  
-
 for(i in u) {  
      info<- sprintf("运行后就会改变 %d%%", round(i*100/length(u)))  
      setTkProgressBar(pb, i*50/length(u), sprintf("小角落也变了 (%s)",info), info)  

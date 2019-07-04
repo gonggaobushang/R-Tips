d242b07bc2e56d864e4bf7d8a66c329e8bf7ef35
gonggaobushang <47070965+gonggaobushang@users.noreply.github.com>
2019-07-04 10:32:59 +0800

    Add files via upload

---
+++ parallel operation.R
@@ -0,0 +1,18 @@
+setwd("C:\\Users\\Administrator.000\\Desktop")
+rm(list = ls())
+library(snowfall)
+library(parallel) 
+detectCores(logical = F) #核个数：parallel 
+sfInit(parallel = TRUE, cpus = detectCores() - 1) #启动核个数
+#cl <- makeCluster(getOption("cl.cores", 3))
+library(ggplot2)
+t1<-data.frame(x=1:100,y=2:101)
+sfLibrary(ggplot2) #载入package
+sfExport("t1") #载入变量
+fun <- function(ipl){
+  gt1<-ggplot(t1)+geom_line(aes(x,y))+xlab(ipl)
+  print(gt1)
+}
+sfLapply(1:10, fun) ->a
+sfStop() #关闭多核
+
